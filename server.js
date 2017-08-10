dbf080bf473048d9cbde13b15faa66afb12bafe4
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:02:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -94,6 +94,12 @@ app.get('/:articlename', function (req, res) {
  res.send (createTemplate(articles[articlename]));
 });
 
+var counter=0;
+app.get('/counter', function (req, res) {
+ counter = counter + 1;
+ res.send (counter.toString());
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
