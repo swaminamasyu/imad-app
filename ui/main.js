010265ebf44009e64cb8d0cebe1416ef3eb6dcf5
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 15:55:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
 
 var img = document.getElementById('madi');
-
-img.onClick = function () {
+img.onclick = function () {
   img.style.marginleft = '100px';  
 };
