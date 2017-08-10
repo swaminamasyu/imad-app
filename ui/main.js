70341626448a0c6739582811256db9d5969e8f06
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:26:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,7 @@ button.onclick = function (){
     
     var request = new XMLHttpRequest();
     
-    request.onreadystatechange = fuction () {
+    request.onreadystatechange = function () {
       if (request.readystate === XMLHttpRequest.DONE) {
             if (request.status === 200) {
                 var counter = request.responseText;
@@ -11,7 +11,7 @@ button.onclick = function (){
                 span.innerHTML = counter.toString();
             }
          } 
-     };
+     }
      
      request.open ('GET','http://swaminamasyu82.imad.hasura-app.io/counter',true);
      request.send (null);
