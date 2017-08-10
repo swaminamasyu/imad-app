63784ff33fd35ace97c399809dca450af8a02701
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:53:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,8 +11,26 @@ button.onclick = function (){
                 span.innerHTML = counter.toString();
             }
          } 
-     }
+     };
      
      request.open ('GET','http://swaminamasyu82.imad.hasura-app.io/counter',true);
      request.send (null);
+};
+
+//submit name
+var nameInput = document.getElementById ('name');
+var name = nameInput.value;
+
+var submit = document.getElementById ('submit_btn');
+submit.onclick = function () {
+    
+    var names = ['name1','name2','name3','name4','name5'];
+    var list = '';
+    
+    for (var i=0; i<names.length; i++) {
+        list = '<li>' + names[i] + '</li>'; 
+    }
+    
+    var ul = document.getElementById ('namelist');
+    ul.innerHTML = list;
 };
