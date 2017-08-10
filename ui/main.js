aed66f47f796410d36795731dc6e32e0a2d2656f
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:13:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function (){
-    count = count + 1;
+    counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-}
+};
