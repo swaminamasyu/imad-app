64d55cfe66a857943d1e12662f8094db9def3888
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:57:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,10 +18,10 @@ button.onclick = function (){
 };
 
 //submit name
-var nameInput = document.getElementById ('name');
+var nameInput = document.getElementById('name');
 var name = nameInput.value;
 
-var submit = document.getElementById ('submit_btn');
+var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
     var names = ['name1','name2','name3','name4'];
