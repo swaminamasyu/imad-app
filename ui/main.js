0aa84ec28898e2a0ed1a73c6c49341d8b1a1542f
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-10 20:54:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -24,11 +24,11 @@ var name = nameInput.value;
 var submit = document.getElementById ('submit_btn');
 submit.onclick = function () {
     
-    var names = ['name1','name2','name3','name4','name5'];
+    var names = ['name1','name2','name3','name4'];
     var list = '';
     
     for (var i=0; i<names.length; i++) {
-        list = '<li>' + names[i] + '</li>'; 
+        list == '<li>' + names[i] + '</li>'; 
     }
     
     var ul = document.getElementById ('namelist');
