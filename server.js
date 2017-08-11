5793014b6a78d964ad103290ce48c50681c03676
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-11 11:53:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -95,6 +95,14 @@ app.get('/counter', function (req, res) {
  res.send (counter.toString());
 });
 
+var names = [];
+app.get('/submit-name', function (req, res) {
+    var name = req.query.name;
+    names.push(name);
+    res.send (JSON.stringify(names));
+});
+
+
 app.get('/:articlename', function (req, res) {
  var articlename = req.params.articlename;
  res.send (createTemplate(articles[articlename]));
@@ -108,12 +116,6 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-var names = [];
-app.get('/submit-name/:name', function (req, res) {
-    var name = req.params.name;
-    names.push(name);
-    res.send (JSON.stringify(names));
-});
 
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
