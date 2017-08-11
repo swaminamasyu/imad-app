4705a6b681252f6fe25894586e0570fe1054f749
swaminamasyu <30682222+swaminamasyu@users.noreply.github.com>
2017-08-11 12:06:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,12 +23,24 @@ var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
-    var names = ['name1','name2','name3','name4'];
-    var list = '';
+    var request = new XMLHttpRequest();
     
-    for (var i=0; i<names.length; i++) {
-        list += '<li>' + names[i] + '</li>'; 
-    }
-    var ul = document.getElementById('namelist');
-    ul.innerHTML = list;
+    request.onreadystatechange = function () {
+      if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                    var names = request.responseText;
+                    names = JSON.parse(names);
+                    var list = '';
+                    
+                    for (var i=0; i<names.length; i++) {
+                        list += '<li>' + names[i] + '</li>'; 
+                    }
+                    var ul = document.getElementById('namelist');
+                    ul.innerHTML = list;
+            }
+         } 
+     };
+     
+     request.open ('GET','http://swaminamasyu82.imad.hasura-app.io/submit-name?name='+name,true);
+     request.send (null);
 };
